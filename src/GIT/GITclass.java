20f28d4a2d39c75bbbab3e6a74bfb0f93edc11ba
CDAC New Delhi <mohnishpali19@gmail.com>
2021-11-23 15:23:52 +0530

    The first commit is done

---
+++ src/GIT/GITclass.java
@@ -0,0 +1,13 @@
+package GIT;
+
+public class GITclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello git");
+		System.out.println("first modify");
+		System.out.println("second modify");
+
+	}
+
+}
